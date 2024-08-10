50a9990e4629625d18d4d79d7c79ef59589a28dc
jadhavswaraj <126857207+jadhavswaraj@users.noreply.github.com>
2024-08-10 19:54:35 +0530

    Add files via upload

---
+++ src/data/navbar-links.js
@@ -0,0 +1,18 @@
+export const NavbarLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Catalog",
+    path: '/catalog',
+  },
+  {
+    title: "About Us",
+    path: "/aboutus",
+  },
+  {
+    title: "Contact Us",
+    path: "/contact",
+  },
+];
